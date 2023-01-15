dc0335689afd5fa9047f30973af2352df7f8bc46
evanjscallan <evanjscallan@gmail.com>
2023-01-15 13:09:38 -0600

    Refactored GlobalVars to become utils.js; Inplemented pure functions into utils.js; Cleaned overall syntax; Used list mapping for JSX inside of Calculator component and restructured stated.

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,9 +1,7 @@
 import React, {useState, useContext} from 'react';
 import Calculator from './Calculator'
 import RenderLineChart from './Chart';
-import UserInput from './UserInput';
 import './../App.css';
-
 const Index = () => {
     return(
         <>
@@ -12,9 +10,8 @@ const Index = () => {
             <h1>API Load Test</h1>
             <h2>Response Times & General Insights</h2>
         </div>
-        <Calculator/>
-        {RenderLineChart}
-        <UserInput/>
+            <Calculator/>
+            <RenderLineChart/>
         </div>
         </>
     )
